068c1cafe7f98a58b48ee36c1788cbb433b3e5b8
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-12 17:45:20 -0500

    Se agregan las importaciones necesarias para la clase interfaz ProveedorRepository

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProveedorRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProveedorRepository.java
@@ -1,4 +1,16 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.repository;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repositorio para la entidad Proveedor.
+ * Este repositorio proporciona operaciones CRUD básicas para la entidad Proveedor
+ * utilizando Spring Data JPA.
+ */
+
 public interface ProveedorRepository {
 }
