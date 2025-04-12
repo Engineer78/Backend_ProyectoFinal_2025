9d3af3f7b87a6c72f10ab0d29ab8d9d85ae24039
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-12 18:00:45 -0500

    Se agrega la importación necesaria para la clase ProveedorConsultaDTO

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoConsultaDTO.java
@@ -0,0 +1,6 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Producto;git
+
+public class ProductoConsultaDTO {
+}
