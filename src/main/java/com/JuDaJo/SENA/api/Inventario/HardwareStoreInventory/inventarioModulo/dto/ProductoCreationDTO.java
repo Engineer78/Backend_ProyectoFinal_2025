68de9bb4b77ad6ac81fb08e1b0796cda82ea9b19
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-12 18:27:32 -0500

    Se crean las importaciones necesarias para la clase ProductoCreationDTO

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoCreationDTO.java
@@ -0,0 +1,9 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class ProductoCreationDTO {
+}
