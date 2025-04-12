dd36acfa88d5ea2708ad9ff92fba6e6f209b45ea
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-12 18:05:27 -0500

    Se declaran los atributos de la clase ProductoConsultaDTO, para representar los datos relacionados con el producto en la transferencia entre capas

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoConsultaDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoConsultaDTO.java
@@ -3,4 +3,15 @@ package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.d
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Producto;git
 
 public class ProductoConsultaDTO {
+
+    // Atributos de la clase ProductoConsultaDTO
+    // Estos atributos son los que se van a mostrar en la consulta de productos
+    private Integer idProducto;
+    private String nombreProducto;
+    private Integer cantidad; // Existencias
+    private Double valorUnitarioProducto;
+    private Double valorTotalProducto;
+    private String proveedor; // Nombre del proveedor
+    private String nitProveedor; // NIT del proveedor
+    private String imagen; // Base64 de la imagen
 }
