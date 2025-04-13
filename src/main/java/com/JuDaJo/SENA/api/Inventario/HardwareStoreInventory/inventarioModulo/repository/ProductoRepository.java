cfba8e77eff3768b842325aeff476ea690957e72
EliteBoku <devijunior3223@gmail.com>
2025-04-13 18:12:20 -0500

    Se añaden las importaciones de: la clase Producto, la anotación NotNull, la interfaz JpaRepository, las anotaciones Query y Param, la anotación Repository, y las clases List y Optional, que permiten definir repositorios, validar campos y realizar consultas personalizadas en la base de datos.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProductoRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProductoRepository.java
@@ -1,4 +1,16 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.repository;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Producto;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ProductoRepository {
+    
 }
