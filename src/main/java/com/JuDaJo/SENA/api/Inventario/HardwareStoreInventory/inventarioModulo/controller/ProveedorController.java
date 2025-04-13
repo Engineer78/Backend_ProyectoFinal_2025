18cd4834d413d97dcd8abb7cfe0bbd1bd93952cd
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-12 19:05:23 -0500

    Se crean las importaciones necesarias para la clase ProveedorController, con esta clase se implementa un controlador REST, para manejar solicitudes HTTP(GET, POST, PUT, DELETE) enviando respuestas en formato JSON.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/controller/ProveedorController.java
@@ -0,0 +1,21 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.controller;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto.ProveedorDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Proveedor;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.ProveedorRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+// Controlador REST para gestionar proveedores
+// Permite realizar operaciones CRUD sobre la entidad Proveedor
+// @RestController indica que esta clase es un controlador REST y maneja las peticiones HTTP
+
+
+public class ProveedorController {
+}
