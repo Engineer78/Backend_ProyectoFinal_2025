82d9956dc4ebe42deddb2fc5ca6bcfcc43aa114f
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-12 23:58:20 -0500

    Se crea la clase ProductoProveedorDTO para gestionar la información de ProductoProveedor

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoProveedorDTO.java
@@ -0,0 +1,10 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * DTO para gestionar la información de ProductoProveedor.
+ */
+public class ProductoProveedorDTO {
+}
