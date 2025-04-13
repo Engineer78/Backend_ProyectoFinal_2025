b688f51d3bf68839597f8381b52669cfaf7fd2b3
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-12 22:56:29 -0500

    Se crea la clase ProductoProveedor con sus respectivas librerías

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/model/ProductoProveedor.java
@@ -0,0 +1,12 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.model;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+/**
+ * Clase que representa la relación entre un producto y un proveedor.
+ * Modela una asociación entre un producto y el proveedor que lo suministra, junto con el precio de compra.
+ */
+@Entity
+public class ProductoProveedor {
+}
