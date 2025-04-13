d1917d63abc2f78eaf683fabb10452683275f11c
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-13 01:34:12 -0500

    Se crea la clase CategoriaController con sus respectivas importaciones

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/controller/CategoriaController.java
@@ -0,0 +1,19 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.controller;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto.CategoriaDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Categoria;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.CategoriaRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173") // Permite peticiones desde tu frontend
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+}
