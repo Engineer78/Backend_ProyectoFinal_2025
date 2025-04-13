832333444217ed70e4c5deb9caa52db193c4efdc
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-12 23:45:36 -0500

    Se crea la clase CategoriaDTO con sus respectivas librerías

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/CategoriaDTO.java
@@ -0,0 +1,7 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CategoriaDTO {
+}
