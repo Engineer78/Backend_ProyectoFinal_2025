4d2796105075ebbce7fd5b101c6d0009e934cf27
EliteBoku <devijunior3223@gmail.com>
2025-04-13 17:46:32 -0500

    Se añaden las importaciones de: la clase ProductoProveedor, la interfaz JpaRepository, la anotación Repository y la clase Optional de JDK, que van a permitir realizar transacciones en la base de datos.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProductoProveedorRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProductoProveedorRepository.java
@@ -1,4 +1,9 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.repository;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.ProductoProveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
 public interface ProductoProveedorRepository {
 }
