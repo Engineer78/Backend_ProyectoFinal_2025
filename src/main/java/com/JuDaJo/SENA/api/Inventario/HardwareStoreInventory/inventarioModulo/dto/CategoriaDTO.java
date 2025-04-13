15659e04cbe1d8e21616f141492e3343aea371d5
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-12 23:47:49 -0500

    Se crea la validación condicional: Solo requerido si idCategoria es nulo

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/CategoriaDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/CategoriaDTO.java
@@ -4,4 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class CategoriaDTO {
+
+    // Validación condicional: Solo requerido si idCategoria es nulo
+    @NotBlank(message = "El nombre de la categoría es obligatorio")
+    @Size(max = 100, message = "El nombre de la categoría no puede exceder los 100 caracteres")
+    private String nombreCategoria;
 }
