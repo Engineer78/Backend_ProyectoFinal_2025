f8c2668a623fcbad3dcd53dfebe5effbabf5de2e
EliteBoku <devijunior3223@gmail.com>
2025-04-13 18:02:39 -0500

    Se añade la anotación @Repository para indicar que la interfaz forma parte de la capa de acceso a datos y permitir que Spring la detecte y maneje automáticamente.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProductoProveedorRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/repository/ProductoProveedorRepository.java
@@ -4,6 +4,6 @@ import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.ProductoProve
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
-
+@Repository
 public interface ProductoProveedorRepository {
 }
