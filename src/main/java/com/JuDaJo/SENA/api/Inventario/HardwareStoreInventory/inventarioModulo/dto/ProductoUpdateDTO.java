b9f49c762998dccab3b732c3307bad1e6b69a274
EliteBoku <devijunior3223@gmail.com>
2025-04-13 21:44:37 -0500

    se agregan importaciones de anotaciones Jakarta (@NotBlank, @NotNull, @Size, @Min y @Lob) para validaciones y persistencia con la base de datos

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoUpdateDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoUpdateDTO.java
@@ -1,4 +1,9 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.Min; // Esta importación también es de Jakarta
 
 public class ProductoUpdateDTO {
 }
