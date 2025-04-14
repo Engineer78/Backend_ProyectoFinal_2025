daffd00a64f60fbe7d7c6e1f355e8330543580a6
EliteBoku <devijunior3223@gmail.com>
2025-04-13 22:30:54 -0500

    Se definen los atributos del DTO Producto, incluyendo validaciones con anotaciones Jakarta (@NotNull, @NotBlank, @Positive y @PositiveOrZero). Se añade el campo  para representar el valor calculado del producto, así como campos relacionados con categoría y proveedor.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoDTO.java
@@ -17,4 +17,34 @@ import java.util.stream.Collectors;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
 public class ProductoDTO {
+    private int idProducto;
+
+    @NotNull(message = "El código del producto no puede ser nulo")
+    private Integer codigoProducto;
+
+    @NotBlank(message = "El nombre del producto no puede estar vacío")
+    private String nombreProducto;
+
+    @Positive(message = "La cantidad debe ser un número positivo")
+    private int cantidad;
+
+    @PositiveOrZero(message = "El valor unitario debe ser mayor o igual a cero")
+    private double valorUnitarioProducto;
+
+    private double valorTotalProducto; // Nuevo campo para el valor total del producto
+
+    private String nombreCategoria;
+
+    private Integer idProveedor;
+
+    private String nombreProveedor;
+
+    private String nitProveedor;
+
+    private String direccionProveedor;
+
+    private String telefonoProveedor;
+
+    private List<Integer> productoProveedores;
+
 }
