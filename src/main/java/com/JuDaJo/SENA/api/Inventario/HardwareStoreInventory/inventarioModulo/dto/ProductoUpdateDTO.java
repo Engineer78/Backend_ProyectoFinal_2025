7220f0b52031a461e9f97e42f96196834806a0bd
EliteBoku <devijunior3223@gmail.com>
2025-04-13 21:48:43 -0500

    Se agregan los atributos del producto, categoría y proveedor en la clase ProductoUpdateDTO , incluyendo campos como código de producto, nombre, cantidad, valor unitario, nombre de categoría, imagen y detalles del proveedor. Además, se implementan validaciones con las anotaciones @NotNull, @NotBlank, @Size y @Min para asegurar la integridad de los datos, y se utiliza @Lob para manejar el campo de imagen como un objeto de gran tamaño (Large Object).

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoUpdateDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoUpdateDTO.java
@@ -6,4 +6,34 @@ import jakarta.persistence.Lob;
 import jakarta.validation.constraints.Min; // Esta importación también es de Jakarta
 
 public class ProductoUpdateDTO {
+    @NotNull(message = "El código del producto no puede ser nulo")
+    private Integer codigoProducto;
+
+    @NotBlank(message = "El nombre del producto no puede estar en blanco")
+    private String nombreProducto;
+
+    @Min(value = 0, message = "La cantidad no puede ser negativa")
+    private int cantidad;
+
+    @Min(value = 0, message = "El valor unitario no puede ser negativo")
+    private double valorUnitarioProducto;
+
+    @NotBlank(message = "El nombre de la categoría es obligatorio")
+    @Size(max = 255, message = "El nombre de la categoría no puede exceder los 255 caracteres")
+    private String nombreCategoria;
+
+    @Lob // Indica que es un campo de gran tamaño (Large Object)
+    private String imagen;
+
+    @NotNull
+    private Integer idProveedor;
+
+    @NotBlank
+    private String nitProveedor;
+
+    @NotBlank
+    private String direccionProveedor;
+
+    @NotBlank
+    private String telefonoProveedor;
 }
