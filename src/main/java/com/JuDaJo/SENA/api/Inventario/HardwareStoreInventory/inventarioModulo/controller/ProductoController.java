ea2d85b361007c8827078bf2859f849f52f97b98
EliteBoku <devijunior3223@gmail.com>
2025-04-13 22:46:51 -0500

    Se declaran las dependencias ProductoRepository, ProductoProveedorRepository, CategoriaRepository y ProveedorRepository como atributos.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/controller/ProductoController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/controller/ProductoController.java
@@ -28,5 +28,8 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/productos")
 
 public class ProductoController {
-    
+    private final ProductoRepository productoRepository;
+    private final ProductoProveedorRepository productoProveedorRepository;
+    private final CategoriaRepository categoriaRepository;
+    private final ProveedorRepository proveedorRepository;
 }
