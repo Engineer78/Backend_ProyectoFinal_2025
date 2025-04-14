93881478da6e973db3776f4003c7a602ba38631e
EliteBoku <devijunior3223@gmail.com>
2025-04-13 22:44:17 -0500

    Se crea el controlador REST ProductoController con las importaciones necesarias para gestionar operaciones CRUD sobre productos, incluyendo relaciones con entidades relacionadas como Categoría, Proveedor y ProductoProveedor.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/controller/ProductoController.java
@@ -0,0 +1,32 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.controller;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.dto.*;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Categoria;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Producto;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.ProductoProveedor;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Proveedor;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.CategoriaRepository;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.ProductoProveedorRepository;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.ProductoRepository;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.repository.ProveedorRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Controlador REST para gestionar los productos en el inventario.
+ * Proporciona funcionalidades como crear, actualizar, eliminar y consultar productos.
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:5173/")
+@RequestMapping("/api/productos")
+
+public class ProductoController {
+    
+}
