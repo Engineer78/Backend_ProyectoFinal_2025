9365a8e32e557e314fd1567b5b37b7d1f32b4f92
EliteBoku <devijunior3223@gmail.com>
2025-04-13 20:21:29 -0500

    Se crea el constructor vacío para la clase proveedor

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/model/Proveedor.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/model/Proveedor.java
@@ -52,4 +52,9 @@ public class Proveedor {
      */
     @OneToMany(mappedBy = "proveedor", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductoProveedor> productoProveedores = new ArrayList<>();
+    /**
+     * Constructor vacío requerido por JPA.
+     */
+    public Proveedor() {
+    }
 }
