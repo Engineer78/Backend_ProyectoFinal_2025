b1bfaea90f85de952e2438d261d13dea270fa306
EliteBoku <devijunior3223@gmail.com>
2025-04-13 21:33:50 -0500

    Se añaden las importaciones necesarias y la documentación inicial en la clase ProveedorDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProveedorDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProveedorDTO.java
@@ -1,4 +1,9 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+/**
+ * DTO para gestionar la información de un proveedor.
+ */
 public class ProveedorDTO {
 }
