175ea7b24ae2a3b7c75a136e9b440b01ab13819d
EliteBoku <devijunior3223@gmail.com>
2025-04-13 22:29:01 -0500

    Se agrega el DTO de Producto con las importaciones necesarias, incluyendo validaciones con anotaciones de Jakarta (@NotBlank, @NotNull, @Positive, @PositiveOrZero) y la anotación @JsonInclude para omitir campos nulos en la serialización JSON.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/dto/ProductoDTO.java
@@ -1,4 +1,20 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.dto;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.model.Producto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * DTO (Data Transfer Object) para la entidad Producto.
+ * Este DTO se utiliza para transferir datos de la entidad Producto entre la capa de servicio y la capa de presentación.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
 public class ProductoDTO {
 }
