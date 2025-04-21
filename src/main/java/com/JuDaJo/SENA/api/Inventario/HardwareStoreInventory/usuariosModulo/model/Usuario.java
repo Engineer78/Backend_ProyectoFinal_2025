5e827cbdfd4071ab5fbe40d3a21a7df4050324e0
EliteBoku <devijunior3223@gmail.com>
2025-04-20 22:14:38 -0500

    Se reorganizan anotaciones colocando @Getter y @Setter debajo de @Entity en la clase Usuario ubicada en el package model.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Usuario.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Usuario.java
@@ -2,6 +2,9 @@ package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.mod
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Objects;
 
 /*
@@ -10,7 +13,8 @@ import java.util.Objects;
  * Esta clase será utilizada para mapear los registros de la tabla correspondiente a usuarios.
  */
 @Entity
-
+@Getter
+@Setter
 public class Usuario {
     /*
     * Identificador Unico de Usuario
