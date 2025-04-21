73865e7f44577e669d161a76a9da541da39e7352
EliteBoku <devijunior3223@gmail.com>
2025-04-20 20:48:52 -0500

    Se agrega la anotación @Id con generación automática al campo idUsuario y se crea la entidad con la notación @Entity en la clase Usuario ubicada en el package model.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/model/Usuario.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/inventarioModulo/model/Usuario.java
@@ -1,6 +1,23 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.inventarioModulo.model;
 
-public class Usuario {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-    
+/*
+ * La anotación @Entity indica que la clase Usuario representa una entidad persistente,
+ * es decir, una tabla dentro de la base de datos gestionada por JPA (Jakarta Persistence API).
+ * Esta clase será utilizada para mapear los registros de la tabla correspondiente a usuarios.
+ */
+@Entity
+public class Usuario {
+    /*
+    * Identificador Unico de Usuario
+    * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUsuario;
+
+
 }
