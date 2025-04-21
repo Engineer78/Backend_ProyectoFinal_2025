57ae553b1f469bd5383869323bdba4d85189acd0
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-20 19:21:49 -0500

    Se crea el @Entity para la clase empleado, y se crea el @Id como el identificador único del empleado.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Empleado.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Empleado.java
@@ -1,4 +1,26 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Esta clase representa un empleado dentro de usuarios.
+ */
+
+@Entity
 public class Empleado {
+
+    /**
+     * Identificador único del empleado.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEmpleado;
+
 }
