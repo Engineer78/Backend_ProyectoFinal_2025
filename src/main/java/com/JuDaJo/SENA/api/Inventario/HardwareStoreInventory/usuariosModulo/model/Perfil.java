dd5c9f0934d31aaad6e03993741bb1544fe218c9
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-20 19:18:10 -0500

    Se hacen las importaciones para la clase, se declara la entidad perfil y se crea el identificador único en la clase Perfil

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Perfil.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Perfil.java
@@ -1,4 +1,20 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Entidad que representa un perfil dentro de usuarios.
+ */
+@Entity
 public class Perfil {
+
+    /**
+     * Identificador único del perfil.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPerfil;
+
 }
