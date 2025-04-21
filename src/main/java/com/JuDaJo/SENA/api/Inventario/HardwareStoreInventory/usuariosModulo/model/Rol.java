adac011fa98be38c0f4b92d313df50f528bbacc6
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-20 21:51:11 -0500

    Se crea la clase Rol.model con las importaciones

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Rol.java
@@ -0,0 +1,18 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Entidad que representa un rol dentro del sistema.
+ */
+
+@Entity
+public class Rol {
+
+    
+
+}
