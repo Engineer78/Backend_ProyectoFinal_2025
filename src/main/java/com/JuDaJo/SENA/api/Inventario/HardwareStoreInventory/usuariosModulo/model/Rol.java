655947a33a9fd9ccd08acb2f5b63c0ad8175af79
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-20 23:01:02 -0500

    Se refactorizan los import de las importaciones lombok por lombok.* en la clase Rol.model

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Rol.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Rol.java
@@ -3,10 +3,7 @@ package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.mod
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * Entidad que representa un rol dentro del sistema.
