a52b51eb11b37c6f1fb20b46fa9f0e90f45d1f72
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-21 23:37:41 -0500

    Se eliminan la validaciones innecesarias @NotBlank y @Size con sus respectivas importaciones en la clase RolDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/RolDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/RolDTO.java
@@ -1,8 +1,5 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 /**
  * DTO para la entidad Rol.
  */
@@ -10,8 +7,6 @@ public class RolDTO {
 
     private Integer idRol;
 
-    @NotBlank(message = "El nombre del rol no puede estar en blanco")
-    @Size(max = 20, message = "El nombre del rol no puede exceder los 20 caracteres")
     private String nombreRol;
 
     private Integer idPerfil; // Referencia al ID del perfil asociado
