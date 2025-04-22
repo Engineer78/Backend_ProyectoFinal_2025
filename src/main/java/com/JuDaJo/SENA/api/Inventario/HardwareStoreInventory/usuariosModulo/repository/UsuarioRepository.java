b09d600e3307b8884796130592d9cb6ed95f2efb
EliteBoku <devijunior3223@gmail.com>
2025-04-21 21:38:49 -0500

    Se crea repositorio JPA para la entidad Usuario en la package repository

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/UsuarioRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/UsuarioRepository.java
@@ -1,4 +1,7 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository;
 
-public interface UsuarioRepository {
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 }
