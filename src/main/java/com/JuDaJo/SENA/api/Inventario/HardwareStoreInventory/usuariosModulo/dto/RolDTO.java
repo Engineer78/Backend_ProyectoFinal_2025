727e25cf616664baa40289e2480e4793bdc185a1
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-21 23:10:47 -0500

    Se declaran los atributos en la clase RolDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/RolDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/RolDTO.java
@@ -1,4 +1,20 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO para la entidad Rol.
+ */
 public class RolDTO {
+
+    private Integer idRol;
+
+    @NotBlank(message = "El nombre del rol no puede estar en blanco")
+    @Size(max = 20, message = "El nombre del rol no puede exceder los 20 caracteres")
+    private String nombreRol;
+
+    private Integer idPerfil; // Referencia al ID del perfil asociado
+
+
 }
