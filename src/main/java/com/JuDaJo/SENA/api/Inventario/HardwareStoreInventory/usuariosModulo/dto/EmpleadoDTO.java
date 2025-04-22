10ed46b5abb237b17695bf92fd621f41f441d3e2
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-21 23:04:41 -0500

    Se declaran los atributos de la clase EmpleadoDTO del package moduloUsuarios.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/EmpleadoDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/EmpleadoDTO.java
@@ -1,4 +1,18 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
 
 public class EmpleadoDTO {
+
+    /**
+     * Se declaran los atributos de la clase EmpleadoDTO.
+     */
+    private int idEmpleado;
+    private String numeroDocumento;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String telefonoMovil;
+    private String direccionResidencia;
+    private String contactoEmergencia;
+    private String telefonoContacto;
+    private int idUsuario; //Se usa solo ID, no todo el objeto usuario.
 }
