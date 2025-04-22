b23876901df5a5d452e92ea40a0626120e1646e1
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-21 23:26:56 -0500

    Se crea la case PerfilDTO la cual permite exponer las entidades al frontend controlando los datos que se envían/reciben dentro del package usuariosModulo del proyecto.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/PerfilDTO.java
@@ -0,0 +1,11 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO para la entidad Perfil.
+ * Contiene solo los datos necesarios que se enviarán o recibirán desde el frontend.
+ */
+public class PerfilDTO {
+}
