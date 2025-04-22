99a2e2c04f752e8a0b3f1666c560d8d3031d24e4
EliteBoku <devijunior3223@gmail.com>
2025-04-22 18:51:49 -0500

    Se añaden las anotaciones correspondientes para la validación de los datos recibidos desde el frontend, y se elimina la anotación @Getter junto con su importación.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/UsuarioDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/UsuarioDTO.java
@@ -1,4 +1,25 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+
+@Builder
 public class UsuarioDTO {
+    // Validaciones
+    @NotEmpty(message = "El nombre del usuario no puede ser nulo")
+    @Size(min =3, max =50, message="El nombre debe estar entre 3 y 50 caracteres")
+    private String nombre;
+    @NotEmpty(message = "La contraseña del usuario no puede estar vacia")
+    @Size(min=6, message = "La contraseña debe tener como minimo 6 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'<>,.?/]).+$",
+            message = "La contraseña debe contener al menos una letra mayúscula y un carácter especial"
+    )
+    private String constraseña;
+    @NotNull( message="Debe seleccionar el rol")
+    private Integer rol;
 }
