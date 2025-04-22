cfb99397847d755f3bee33bc25207e8635b1c8b0
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-22 00:14:36 -0500

    Se eliminan las iportaciones NotBlank y Size en la clase PerfilDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/PerfilDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/PerfilDTO.java
@@ -1,8 +1,5 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 /**
  * DTO para la entidad Perfil.
  * Contiene solo los datos necesarios que se enviarán o recibirán desde el frontend.
