46958c17d33413ecb14a126a23280bd787de59b7
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-21 20:09:34 -0500

    Se ajustan los cometarios javaDoc del código.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Usuario.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Usuario.java
@@ -4,46 +4,49 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.util.Objects;
 
-/*
+/**
  * La anotación @Entity indica que la clase Usuario representa una entidad persistente,
  * es decir, una tabla dentro de la base de datos gestionada por JPA (Jakarta Persistence API).
  * Esta clase será utilizada para mapear los registros de la tabla correspondiente a usuarios.
  */
 @Entity
 public class Usuario {
-    /*
+
+    /**
     * Identificador Unico de Usuario
     * */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idUsuario;
 
-    /*
+    /**
     * Nombre del usuario
     * */
-
     @Column(name = "nombre_usuario")
     @NotNull(message = "El nombre del usuario no puede ser nulo")
     private String nombreUsuario;
-    /*
+
+    /**
     * Contraseña asginada al usuario
     * */
     @Column(name="contraseña_usuario")
     @NotNull(message = "La contraseña del usuario no puede ser nula")
     private String contraseña;
-    /*
+
+    /**
     * Relación entre Usuario y Rol
     * */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_rol", referencedColumnName = "idRol", nullable = false)
-
     @NotNull(message = "El rol no puede ser nulo")
     private Rol rol;
-    /*
+
+    /**
     * Constructor Vacío requerido por el JPA
     * */
     public Usuario() {}
-    /*
+
+    /**
      * Constructor con argumentos para facilitar la creación de instancias
      */
     public Usuario(String nombreUsuario, String contraseña, Rol rol) {
@@ -61,6 +64,7 @@ public class Usuario {
     public int getIdUsuario() {
         return idUsuario;
     }
+
     /**
      * Establece el id
      * @param idUsuario id del usuario.
@@ -68,6 +72,7 @@ public class Usuario {
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
+
     /**
      * Obtiene el nombre de usuario.
      * @return nombreUsuario.
@@ -75,6 +80,7 @@ public class Usuario {
     public String getNombreUsuario() {
         return nombreUsuario;
     }
+
     /**
      * Establece el nombre de usuario
      * @param nombreUsuario Nombre del usuario.
@@ -82,6 +88,7 @@ public class Usuario {
     public void setNombreUsuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
     }
+
     /**
      * Obtiene la contraseña
      * @return contraseña.
@@ -89,6 +96,7 @@ public class Usuario {
     public String getContraseña() {
         return contraseña;
     }
+
     /**
      * Establece la contraseña
      * @param contraseña contraseña del usuario.
@@ -96,6 +104,7 @@ public class Usuario {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
+
     /**
      * Obtiene el rol
      * @return rol.
@@ -103,6 +112,7 @@ public class Usuario {
     public Rol getRol() {
         return rol;
     }
+
     /**
      * Establece el rol
      * @param rol rol del usuario.
@@ -110,6 +120,7 @@ public class Usuario {
     public void setRol(Rol rol) {
         this.rol = rol;
     }
+
     /**
      * Compara usuarios por ID para garantizar unicidad lógica.
      */
