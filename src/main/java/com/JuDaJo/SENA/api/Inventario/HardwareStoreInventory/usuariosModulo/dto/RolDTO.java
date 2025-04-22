3a97c884ec02048988cd189b752031acd79b52d2
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-21 23:17:47 -0500

    Se crean los Getters y Setters para la entidad Rol en la clase RolDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/RolDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/RolDTO.java
@@ -30,4 +30,30 @@ public class RolDTO {
         this.idPerfil = idPerfil;
     }
 
+    /**
+     * Getters y Setters para la entidad Rol.
+     */
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public Integer getIdPerfil() {
+        return idPerfil;
+    }
+
+    public void setIdPerfil(Integer idPerfil) {
+        this.idPerfil = idPerfil;
+    }
 }
