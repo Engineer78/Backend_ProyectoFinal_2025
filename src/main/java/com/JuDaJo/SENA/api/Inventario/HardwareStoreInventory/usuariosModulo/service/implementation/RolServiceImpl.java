bdf41c13a44ccf7d369aee5540bd5873954ac7d1
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-22 18:29:56 -0500

    Se crea la clase RolServiceImpl con anotación @Service e implementación inicial de RolService dentro de usuariosModulo.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/RolServiceImpl.java
@@ -0,0 +1,9 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.RolService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RolServiceImpl implements RolService {
+
+}
