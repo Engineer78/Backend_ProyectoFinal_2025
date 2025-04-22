97dd06484fa196176a30723197cec109765caaba
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-21 23:08:19 -0500

    Se crea el constructor con argumentos de la clase EmpleadoDTO para el package moduloUsuarios.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/EmpleadoDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/EmpleadoDTO.java
@@ -15,4 +15,24 @@ public class EmpleadoDTO {
     private String contactoEmergencia;
     private String telefonoContacto;
     private int idUsuario; //Se usa solo ID, no todo el objeto usuario.
+
+    /**
+     * Se crea el constructor vacío de la clase EmpleadoDTO.
+     */
+    public EmpleadoDTO() {}
+
+    /**
+     * Se crea el constructor con argumentos de la clase EmpleadoDTO.
+     */
+    public EmpleadoDTO(int idEmpleado, String numeroDocumento, String nombres, String apellidoPaterno, String apellidoMaterno, String telefonoMovil, String direccionResidencia, String contactoEmergencia, String telefonoContacto, int idUsuario) {
+        this.idEmpleado = idEmpleado;
+        this.numeroDocumento = numeroDocumento;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.telefonoMovil = telefonoMovil;
+        this.direccionResidencia = direccionResidencia;
+        this.contactoEmergencia = contactoEmergencia;
+    }
+
 }
