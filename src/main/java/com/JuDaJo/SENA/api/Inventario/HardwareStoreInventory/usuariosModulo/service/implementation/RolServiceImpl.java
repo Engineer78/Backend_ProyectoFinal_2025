8f2856fdf27d59cf41fe9d69d40c2a4b9dd2e8f9
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-22 18:37:22 -0500

    Se inyectan las dependencias de RolRepository y PerfilRepository en la clase RolServiceImpl.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/RolServiceImpl.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/RolServiceImpl.java
@@ -1,9 +1,23 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository.RolRepository;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository.PerfilRepository;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RolServiceImpl implements RolService {
 
+    // Aquí se implementarán los métodos definidos en RolService
+    private final RolRepository rolRepository;
+    private final PerfilRepository perfilRepository;
+
+    @Autowired
+    public RolServiceImpl(RolRepository rolRepository, PerfilRepository perfilRepository) {
+        this.rolRepository = rolRepository;
+        this.perfilRepository = perfilRepository;
+    }
+
+    // Aquí se implementarán los métodos del servicio
 }
