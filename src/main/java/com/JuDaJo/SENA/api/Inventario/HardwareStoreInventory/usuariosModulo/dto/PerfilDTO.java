f35532c31083e2647eaff17f01356eca05640634
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-23 18:06:59 -0500

    Se agregan los métodos toString(), equals() y hashCode() en la clase PerfilDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/PerfilDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/PerfilDTO.java
@@ -43,26 +43,66 @@ public class PerfilDTO {
 
     // Getters y Setters
     public Integer getIdPerfil() {
+
         return idPerfil;
     }
 
     public void setIdPerfil(Integer idPerfil) {
+
         this.idPerfil = idPerfil;
     }
 
     public String getNombrePerfil() {
+
         return nombrePerfil;
     }
 
     public void setNombrePerfil(String nombrePerfil) {
+
         this.nombrePerfil = nombrePerfil;
     }
 
     public String getDescripcion() {
+
         return descripcion;
     }
 
     public void setDescripcion(String descripcion) {
+
         this.descripcion = descripcion;
+    }
+
+    /**
+     * Representación en cadena de texto del objeto PerfilDTO.
+     *
+     * @return Una cadena representando los atributos del perfil.
+     */
+    @Override
+    public String toString() {
+        return "PerfilDTO{idPerfil=" + idPerfil + ", nombrePerfil='" + nombrePerfil + "', descripcion='" + descripcion + "'}";
+    }
+
+    /**
+     * Compara dos objetos PerfilDTO para determinar si son iguales.
+     *
+     * @param obj El objeto con el que se comparará.
+     * @return true si los objetos son iguales, false de lo contrario.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        PerfilDTO that = (PerfilDTO) obj;
+        return idPerfil.equals(that.idPerfil);
+    }
+
+    /**
+     * Calcula el valor hash de un objeto PerfilDTO.
+     *
+     * @return El valor hash calculado para el perfil.
+     */
+    @Override
+    public int hashCode() {
+        return idPerfil.hashCode();
     }
 }
