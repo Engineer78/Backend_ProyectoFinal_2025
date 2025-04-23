4bf3550a8f4e44554b77bcfd2185ce88b4494138
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-23 16:56:14 -0500

    Se crea la clase EmpledoController del package moduloUsuarios donde se expondran los endpoints para realizar la peticiones al backend desxde el frontend.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/EmpleadoController.java
@@ -0,0 +1,10 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.EmpleadoDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.EmpleadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class EmpleadoController {
+}
