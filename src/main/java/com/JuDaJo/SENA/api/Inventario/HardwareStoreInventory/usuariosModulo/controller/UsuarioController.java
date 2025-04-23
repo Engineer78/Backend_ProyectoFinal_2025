a25a8d4d6e620a3168f1d6bbc954abccd4cdd74e
EliteBoku <devijunior3223@gmail.com>
2025-04-23 16:48:20 -0500

    Se añade la anotación @RestController para gestionar la creación de usuarios en el módulo usuariosModulo.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/UsuarioController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/UsuarioController.java
@@ -1,4 +1,11 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+* Controlador Rest para gestionar la creación de usuarios en la entidad Empleado
+* Proporcionará la funcionalidad: CrearUsuarios
+* */
+@RestController
 public class UsuarioController {
 }
