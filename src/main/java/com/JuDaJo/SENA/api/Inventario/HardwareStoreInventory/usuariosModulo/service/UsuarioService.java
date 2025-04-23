d9ec7876210ba5b36107e877607b49b21cc7b640
EliteBoku <devijunior3223@gmail.com>
2025-04-22 20:06:55 -0500

    Se añaden los métodos en UserService que la clase UserServiceImpl utilizará para implementar la lógica de gestión de usuarios.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/UsuarioService.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/UsuarioService.java
@@ -1,4 +1,13 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.UsuarioDTO;
+
+import java.util.List;
+
 public interface UsuarioService {
+    UsuarioDTO encontrarPorId(int id);
+    List<UsuarioDTO> listarTodos();
+    UsuarioDTO crear(UsuarioDTO usuarioDTO);
+    UsuarioDTO actualizar(int id, UsuarioDTO usuarioDTO);
+    void eliminar(int id);
 }
