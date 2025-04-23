cfa0e761c1c238bd0f426f490f5b5b7d5062c8e0
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-23 00:29:25 -0500

    Se crea la importación para @Service en la clase PerfilServiceImpl.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/PerfilServiceImpl.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/PerfilServiceImpl.java
@@ -1,5 +1,7 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Implementación del servicio para los perfiles. Contiene la lógica de negocio
  * para crear, obtener, actualizar y eliminar perfiles.
