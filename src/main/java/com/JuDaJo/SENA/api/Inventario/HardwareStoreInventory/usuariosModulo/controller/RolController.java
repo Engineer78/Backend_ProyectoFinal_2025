12435eb856165c02974aca1275294038c4de2353
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-22 22:58:58 -0500

    Se indica que esta clase es un controlador REST en la clase RolController.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/RolController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/RolController.java
@@ -1,4 +1,14 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
 
+// Indica que esta clase es un controlador REST
+@RestController
+@RequestMapping("/api/roles") // Ruta base para todas las peticiones relacionadas con roles
 public class RolController {
+
+    private final RolService rolService;
+
+    // Inyección del servicio RolService a través del constructor
+    public RolController(RolService rolService) {
+        this.rolService = rolService;
+    }
 }
