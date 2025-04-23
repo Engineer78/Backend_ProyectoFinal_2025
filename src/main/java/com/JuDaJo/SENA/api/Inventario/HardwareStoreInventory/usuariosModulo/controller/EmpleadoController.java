7044785d368546ad319decad40db580497c36915
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-23 16:59:46 -0500

    Se crea el controlador REST en la clase EmpleadoController para la gestión de empleados.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/EmpleadoController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/EmpleadoController.java
@@ -6,5 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+/**
+ * Controlador REST para gestionar los empleados.
+ */
+@RestController
+@RequestMapping("/api/empleados")
 public class EmpleadoController {
+
+  
 }
