ecc95996f35a7e31af529614150c17bc28b3af47
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-23 18:38:42 -0500

    Se agrega el método para listar los perfiles en la clase PerfiController.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/PerfilController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/PerfilController.java
@@ -1,8 +1,15 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.PerfilDTO;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.PerfilService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 // Indica que esta clase es un controlador REST
 @RestController
@@ -15,5 +22,15 @@ public class PerfilController {
     public PerfilController(PerfilService perfilService) {
         this.perfilService = perfilService;
     }
+
+    // ================================
+    // Listar todos los perfiles
+    // ================================
+    @GetMapping("/buscar")
+    public ResponseEntity<List<PerfilDTO>> listarPerfiles() {
+        List<PerfilDTO> perfiles = perfilService.listarPerfiles();
+        return new ResponseEntity<>(perfiles, HttpStatus.OK);
+    }
+
 
 }
