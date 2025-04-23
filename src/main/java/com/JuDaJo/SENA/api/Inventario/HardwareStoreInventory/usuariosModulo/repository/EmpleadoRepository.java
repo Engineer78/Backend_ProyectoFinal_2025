a553e4bf03a5ab79616f6498eed3862dadfd4535
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-22 22:16:16 -0500

    Se crean los métodos optional FinByNumeroDocumento y deleteByIdEmpleado para la interfaz EmpleadoRepository del package moduloUsuarios.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/EmpleadoRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/EmpleadoRepository.java
@@ -3,5 +3,20 @@ package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.rep
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+
+    /**
+     * usca un empleado por su número de documento.
+     * @param numeroDocumento El número de documento del empleado a buscar
+     * @return Un Optional que contiene el empleado si se encuentra, o vacío si no existe
+     */
+    Optional<Empleado> findByNumeroDocumento(String numeroDocumento);
+
+    /**
+     * Se utiliza para eliminar un empleado por su ID.
+     * @param idEmpleado El identificador único del empleado a eliminar
+     */
+    void deleteByIdEmpleado(Integer idEmpleado);
 }
