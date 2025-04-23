e6ddce581f56a933a7a5bf2166abf9cd2b6b53ed
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-23 00:02:36 -0500

    Se crea la interface PerfilService.java en el package usuariosModulo.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/PerfilService.java
@@ -0,0 +1,8 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service;
+
+/**
+ * Interfaz para el servicio de perfiles. Define las operaciones básicas
+ * relacionadas con la gestión de perfiles en el sistema.
+ */
+public interface PerfilService {
+}
