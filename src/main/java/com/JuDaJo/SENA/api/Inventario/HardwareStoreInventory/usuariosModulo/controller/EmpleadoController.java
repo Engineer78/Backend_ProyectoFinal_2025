288a6a946549e874626b2810440b40a1f2cfddcf
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-23 17:05:36 -0500

    Se crea el @GetMapping para listar todos los empleados y se crea la importancion java.util.List

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/EmpleadoController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/EmpleadoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 /**
  * Controlador REST para gestionar los empleados.
@@ -15,8 +17,18 @@ import org.springframework.web.bind.annotation.*;
 public class EmpleadoController {
 
     /**
-     * Servicio para gestionar los empleados.
+     * Se inyecta el servicio de EmpleadoService.
+     * @see EmpleadoService
      */
     @Autowired
     private EmpleadoService empleadoService;
+
+    /**
+     * Lista todos los empleados registrados.
+     */
+    @GetMapping
+    public ResponseEntity<List<EmpleadoDTO>> listarEmpleados() {
+        return ResponseEntity.ok(empleadoService.listarEmpleados());
+    }
+
 }
