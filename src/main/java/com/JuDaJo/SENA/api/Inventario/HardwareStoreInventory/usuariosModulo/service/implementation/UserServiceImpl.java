f9ebd2eac89cd6338b6216aa28c02a8bd905d856
EliteBoku <devijunior3223@gmail.com>
2025-04-22 21:28:11 -0500

    Se inyecta la dependencia de UsuarioRepository en UserServiceImpl

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/UserServiceImpl.java
@@ -1,17 +1,23 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.UsuarioDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository.UsuarioRepository;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.UsuarioService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UsuarioService {
     /*
-    * Metodos Abstractos
+    * Inyecccion de dependencias
     * */
-
+    private final UsuarioRepository usuarioRepository;
+    public UserServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+    /*
+     * Metodos Abstractos
+     * */
     @Override
     public UsuarioDTO crear(UsuarioDTO usuarioDTO) {
         return null;
     }
-
 }
