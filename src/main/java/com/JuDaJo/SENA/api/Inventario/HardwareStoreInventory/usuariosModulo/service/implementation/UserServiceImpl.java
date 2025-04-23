76664e6231e46dff4b9c43b7aabe51968f8b33df
EliteBoku <devijunior3223@gmail.com>
2025-04-22 20:18:37 -0500

    Se implementan los métodos en la clase UserServiceImpl ubicada en usuariosModulo/service/implementation.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
 
-public class UserServiceImpl {
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.UsuarioDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.UsuarioService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UsuarioService {
+    @Override
+    public UsuarioDTO encontrarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<UsuarioDTO> listarTodos() {
+        return List.of();
+    }
+
+    @Override
+    public UsuarioDTO crear(UsuarioDTO usuarioDTO) {
+        return null;
+    }
+
+    @Override
+    public UsuarioDTO actualizar(int id, UsuarioDTO usuarioDTO) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(int id) {
+
+    }
 }
