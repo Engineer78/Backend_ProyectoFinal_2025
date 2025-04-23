089b55e483ac257616394999b6ec46e2d975c3c9
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-22 23:00:01 -0500

    Se realizan las importaciones correspondientes en la clase RolController.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/RolController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/RolController.java
@@ -1,5 +1,9 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.RolService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 // Indica que esta clase es un controlador REST
 @RestController
 @RequestMapping("/api/roles") // Ruta base para todas las peticiones relacionadas con roles
