aed5ebb6a9baeb72b307d6fa7381cb9eff79b8f9
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-22 20:25:25 -0500

    Se crea @Autowired para inyectar la dependencia de UsuarioRepository en EmpleadoServiceImpl.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/EmpleadoServiceImpl.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/EmpleadoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
 
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.EmpleadoDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model.Empleado;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model.Usuario;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository.EmpleadoRepository;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository.UsuarioRepository;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.EmpleadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +14,17 @@ import org.springframework.stereotype.Service;
 public class EmpleadoServiceImpl implements EmpleadoService {
 
     /**
-     * Se inyecta la de dependencia para el repositorio de EmpleadoRepository.
+     * Se inyecta la dependencia para el repositorio de EmpleadoRepository.
      */
     @Autowired
     private EmpleadoRepository empleadoRepository;
+
+    /**
+     * Se inyecta la dependecia para el servicio de UsuarioRepository..
+     */
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
 
 
 }
