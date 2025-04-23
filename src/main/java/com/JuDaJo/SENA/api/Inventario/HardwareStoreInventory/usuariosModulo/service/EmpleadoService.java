4f69e0dc135e174c209c4a6ca20b8603aff32d70
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-22 23:10:30 -0500

    Se ajustan nuevamente los @param de EmpleadoService dejando unicamente @param dto.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/EmpleadoService.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/EmpleadoService.java
@@ -8,11 +8,7 @@ public interface EmpleadoService {
     /**
      * Métodos principales de la clase EmpleadoService.
      * Definen el contrato de la lógica de negocio para trabajar con empleados.
-     * @Param crearEmpleado en la base de datos.
-     * @List<EmpleadoDTO> devuelve una lista con todos los empleados existentes en la base de datos.
-     * @Param buscar un empleado por su número Documento.
-     * @Param actualizarEmpleado existente en la base de datos.
-     * @Param eliminarEmpleado elimina un empleado existente en la base de datos por su id.
+     * @param dto Objeto EmpleadoDTO con los datos del empleado a crear.
      */
     EmpleadoDTO crearEmpleado(EmpleadoDTO dto);
     EmpleadoDTO buscarEmpleadoPorDocumento(String numeroDocumento);
