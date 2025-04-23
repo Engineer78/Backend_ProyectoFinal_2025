7536cbec67aab5b7fff1275dac2f87794b986bfc
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-22 23:15:55 -0500

    Se realizan las importaciones correspondientes para @GetMapping en la clase RolController.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/RolController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/RolController.java
@@ -8,10 +8,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Indica que esta clase es un controlador REST
 @RestController
