2e551ac9b6562c30b4f06cf72dd59dfb5249cf76
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-22 21:39:50 -0500

    Se modifica EmpleadoService ajustando los @Param de crear, buscar, actualizar, eliminar y Listar para un empleado.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/EmpleadoService.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/EmpleadoService.java
@@ -8,15 +8,16 @@ public interface EmpleadoService {
     /**
      * Métodos principales de la clase EmpleadoService.
      * Definen el contrato de la lógica de negocio para trabajar con empleados.
-     * @Param EmpleadoDTO crea un nuevo empleado en la base de datos.
-     * @List<EmpleadoDTO> listarEmpleado() devuelve una lista con todos los empleados existentes en la base de datos.
-     * @Param EmpleadoDTO idEmpleado) buscar un empleado por su ID.
-     * @Param EmpleadoDTO actualizarEmpleado(int idEmpleado) actualiza un empleado existente en la base de datos.
-     * @Param eliminarEmpleado(int idEmpleado) elimina un empleado existente en la base de datos.
+     * @Param crearEmpleado en la base de datos.
+     * @List<EmpleadoDTO> devuelve una lista con todos los empleados existentes en la base de datos.
+     * @Param buscar un empleado por su número Documento.
+     * @Param actualizarEmpleado existente en la base de datos.
+     * @Param eliminarEmpleado elimina un empleado existente en la base de datos por su id.
      */
-    EmpleadoDTO crearEmpleado(EmpleadoDTO empleadoDTO);
+    EmpleadoDTO crearEmpleado(EmpleadoDTO dto);
+    EmpleadoDTO buscarEmpleadoPorDocumento(String numeroDocumento);
+    EmpleadoDTO actualizarEmpleado(int idEmpleado, EmpleadoDTO dto);
+    void elimnarEmpledo(int idEmpleado);
     List<EmpleadoDTO> listarEmpleados();
-    EmpleadoDTO buscarPorId(int idEmpleado);
-    EmpleadoDTO actualizarEmpleado(int idEmpleado, EmpleadoDTO empleadoDTO);
-    void eliminarEmpleado(int idEmpleado);
+
 }
