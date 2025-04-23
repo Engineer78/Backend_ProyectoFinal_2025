70dec4c9f0533541dd106906689acf592da9a89a
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-23 00:28:26 -0500

    Se crea la implementación del servicio para los perfiles en la clase PerfilServiceImpl.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/PerfilServiceImpl.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/implementation/PerfilServiceImpl.java
@@ -1,4 +1,9 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.implementation;
 
+/**
+ * Implementación del servicio para los perfiles. Contiene la lógica de negocio
+ * para crear, obtener, actualizar y eliminar perfiles.
+ */
+@Service
 public class PerfilServiceImpl {
 }
