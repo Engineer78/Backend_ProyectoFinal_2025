407a54afca596b58ae197c49d08377978faa9c9f
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-23 18:27:19 -0500

    Se indica que esta clase es un controlador REST en la clase PerfiController.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/PerfilController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/PerfilController.java
@@ -1,4 +1,14 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.PerfilService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Indica que esta clase es un controlador REST
+@RestController
+@RequestMapping("/api/perfiles")
 public class PerfilController {
+
+    private PerfilService perfilService;
+
 }
