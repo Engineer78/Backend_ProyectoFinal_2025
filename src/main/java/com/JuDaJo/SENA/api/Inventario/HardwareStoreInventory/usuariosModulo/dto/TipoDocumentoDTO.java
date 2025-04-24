6019a51703d92d6b51a78d556eb40d3ef60cfe4b
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 01:21:04 -0500

    Se crea el constructor con argumentos de la clase TipoDocuemnto.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/TipoDocumentoDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/TipoDocumentoDTO.java
@@ -14,4 +14,16 @@ public class TipoDocumentoDTO {
      */
     public TipoDocumentoDTO() {
     }
+
+    /**
+     * Se crea un constructor con los atributos de la clase.
+     * @param idTipoDocumento
+     * @param codigo
+     * @param nombre
+     */
+    public TipoDocumentoDTO(Integer idTipoDocumento, String codigo, String nombre) {
+        this.idTipoDocumento = idTipoDocumento;
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
 }
