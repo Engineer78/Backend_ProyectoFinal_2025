e766928608e2b8980305010acc06d4c2458d4359
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 16:45:58 -0500

    Se eliminan espacios innecesarios dentro del código de la clase Empleado.model

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Empleado.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/Empleado.java
@@ -1,11 +1,8 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
-
 
 /**
  * Esta clase representa un empleado dentro de usuarios.
