e78c2fe8cad0215d00c9ff565d0837d50bb7dbe5
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 16:56:28 -0500

    Se crea el @GetMapping para la búsuqeda de todos los tipos de documentos dentro de la base de datos.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/TipoDocumentoController.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/TipoDocumentoController.java
@@ -12,4 +12,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tipo-documento")
 public class TipoDocumentoController {
+
+    /**
+     * Servicio que gestiona las operaciones relacionadas con los tipos de documento.
+     * Este campo es inyectado a través del constructor usando @Autowired para mantener
+     * los principios de inversión de dependencias.
+     */
+    private final TipoDocumentoService tipoDocumentoService;
+
+    @Autowired
+    public TipoDocumentoController(TipoDocumentoService tipoDocumentoService) {
+        this.tipoDocumentoService = tipoDocumentoService;
+    }
+
+    /**
+     * Realiza la búsqueda de todos los tipos de documentos en la base de datos.
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<List<TipoDocumentoDTO>> listarTiposDocumento() {
+        List<TipoDocumentoDTO> lista = tipoDocumentoService.listarTiposDocumento();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
+    }
+
+
+
+
 }
