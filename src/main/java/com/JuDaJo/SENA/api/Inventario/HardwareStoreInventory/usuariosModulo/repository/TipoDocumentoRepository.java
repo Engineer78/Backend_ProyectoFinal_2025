9bfc736dfb29eb9ddac7cf1df984e63d9bc28501
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 01:07:40 -0500

    Se crea la interfaz TipoDocumentoRepository con las importaciones de model y org.Springfreamework

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/TipoDocumentoRepository.java
@@ -0,0 +1,8 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.repository;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model.TipoDocumento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface TipoDocumentoRepository {
+}
