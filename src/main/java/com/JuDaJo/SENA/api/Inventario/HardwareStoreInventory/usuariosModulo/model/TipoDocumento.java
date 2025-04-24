39570832297b131b20b4a56596496beb9b12efbd
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 00:35:19 -0500

    Se agregan las importaciones necesarias a la clase TipoDocuemnto.model

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/TipoDocumento.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/TipoDocumento.java
@@ -1,4 +1,8 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class TipoDocumento {
 }
