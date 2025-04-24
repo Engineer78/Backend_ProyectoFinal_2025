b48e3fd8cc1711ef8d4384b61b3dfcc60ae0a917
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 01:04:35 -0500

    Se asignan los @Getters y @Setters de la clase TipoDocumento.model

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/TipoDocumento.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/model/TipoDocumento.java
@@ -31,4 +31,39 @@ public class TipoDocumento {
     @OneToMany(mappedBy = "tipoDocumento", fetch = FetchType.LAZY)
     private List<Empleado> empleados;
 
+    /**
+     * Se crean los getters y setters para los atributos de la clase.
+     * @return
+     */
+    public Integer getIdTipoDocumento() {
+        return idTipoDocumento;
+    }
+
+    public void setIdTipoDocumento(Integer idTipoDocumento) {
+        this.idTipoDocumento = idTipoDocumento;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
 }
