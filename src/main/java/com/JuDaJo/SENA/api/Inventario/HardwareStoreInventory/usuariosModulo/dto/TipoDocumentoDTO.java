7ff683ab2700cc5c5f9b50574824195d98e988ef
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 01:18:45 -0500

    Se definen los atributos de la clase TipoDocumentoDTO.

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/TipoDocumentoDTO.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/dto/TipoDocumentoDTO.java
@@ -1,4 +1,11 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto;
 
 public class TipoDocumentoDTO {
+
+    /**
+     * Atributos de la clase TipoDocumentoDTO.
+     */
+    private Integer idTipoDocumento;
+    private String codigo;
+    private String nombre;
 }
