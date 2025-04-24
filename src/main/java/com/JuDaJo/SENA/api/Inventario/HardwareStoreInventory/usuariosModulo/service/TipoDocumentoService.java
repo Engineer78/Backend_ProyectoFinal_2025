7bb1021403efd599eeea8fc59b2936edd796d724
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 01:26:19 -0500

    Se crea la interfaz TipoDocumentoService con las importaciones necesarias TipoDocumentoDTO y java.util.list dentro del package moduloUsuarios.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/TipoDocumentoService.java
@@ -0,0 +1,7 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.TipoDocumentoDTO;
+import java.util.List;
+
+public interface TipoDocumentoService {
+}
