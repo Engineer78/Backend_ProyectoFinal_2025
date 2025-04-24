8178cf2fe0b1158e17b63db2ec81ee357adab8bf
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-24 16:47:59 -0500

    Se crea la clase TipoDocumentoController con las importaciones necesarias dentro del package moduloUsuarios.

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/controller/TipoDocumentoController.java
@@ -0,0 +1,13 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.controller;
+
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.TipoDocumentoDTO;
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service.TipoDocumentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class TipoDocumentoController {
+}
