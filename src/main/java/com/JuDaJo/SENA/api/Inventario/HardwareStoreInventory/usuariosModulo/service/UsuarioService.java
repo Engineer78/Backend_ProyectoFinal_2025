76242e6f26b8d81f87c015e4a24efaa5e742af76
Juan Gallego <homertrixmemo@yahoo.com>
2025-07-05 22:27:29 -0500

    refactor: actualiza UsuarioService para incluir cambio de contraseña

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/UsuarioService.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/service/UsuarioService.java
@@ -1,10 +1,13 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.service;
 
+import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.UsuarioChangePasswordDTO;
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.dto.UsuarioDTO;
 
 
 public interface UsuarioService {
 
     UsuarioDTO crear(UsuarioDTO usuarioDTO);
+
+    void cambiarContrasena(String nombreUsuario, UsuarioChangePasswordDTO dto);
 
 }
