65489f8aa71890e7f7b36f62015138dccd58d80a
Juan Gallego <homertrixmemo@yahoo.com>
2025-07-05 21:54:54 -0500

    refactor: reemplaza completamente la interfaz TipoDocumentoRepository

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/TipoDocumentoRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/TipoDocumentoRepository.java
@@ -4,6 +4,11 @@ import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.mode
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repositorio para la entidad TipoDocumento.
+ * Este repositorio proporciona operaciones CRUD básicas para la entidad TipoDocumento
+ * utilizando Spring Data JPA.
+ */
 @Repository
 public interface TipoDocumentoRepository extends JpaRepository<TipoDocumento, Integer> {
 }
