141c61bc1e6a6c1ccb0d93a524208f89b3b4bc71
Juan Gallego <homertrixmemo@yahoo.com>
2025-07-05 22:00:10 -0500

    refactor: reemplaza completamente UsuarioRepository e incluye método findByNombreUsuario con JOIN FETCH para login con JWT

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/UsuarioRepository.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/usuariosModulo/repository/UsuarioRepository.java
@@ -2,6 +2,10 @@ package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.rep
 
 import com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.usuariosModulo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
@@ -11,4 +15,13 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
      * @return
      */
     boolean existsByNombreUsuario(String nombreUsuario);
+
+    /**
+     * Se utiliza para buscar un usuario por su nombre de usuario necesario para JWT y login.
+     * @param nombreUsuario
+     * @return
+     */
+    @Query("SELECT u FROM Usuario u JOIN FETCH u.rol JOIN FETCH u.empleado WHERE u.nombreUsuario = :nombreUsuario")
+    Optional<Usuario> findByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
+
 }
